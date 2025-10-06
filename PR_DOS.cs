a182b9a2090f0563554d7d9a35802bed6c2faccc
aiko <mildxw@gmail.com>
2025-10-06 18:17:09 +0300

    Модель товара + категории

---
+++ PR_DOS.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP423_Bogdanova
+{
+    internal class PR_DOS
+    {
+    }
+}
